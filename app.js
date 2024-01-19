958abda3672aefa266b36698271b6b3f0d2af4ae
Muhammad Mustafa Khan <137421729+Mustafa9106@users.noreply.github.com>
2024-01-19 16:18:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+alert('Greetings! Welcome to this page')
+
+var name = prompt('What is your name?')
+var age = prompt('What is your age?')
+
+console.log(name);
+console.log(age);
+
+if (age < 18) {
+    console.log('You are a student');
+}
+else{
+    console.log('You are not a student');
+}
